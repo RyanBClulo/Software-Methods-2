20cfb8b0ba56479cae814983b9de054224ba2069
RyanBClulo <ryan.clulo@marquette.edu>
2016-03-31 14:35:28 -0500

    1st

---
+++ Software Methodologies Project/src/window/ImageLoader.java
@@ -0,0 +1,21 @@
+package window;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageLoader {
+
+	public static BufferedImage loadImage(String path){
+		try {
+			//return the buffered image
+			return ImageIO.read(ImageLoader.class.getResource(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return null;
+	}
+	
+}
---
+++ Software Methodologies Project/src/window/LevelControl.java
@@ -0,0 +1,32 @@
+package window;
+
+import java.util.Iterator;
+import java.util.Random;
+
+import phisicsObjects.Enemy;
+
+public class LevelControl {
+	private static int currentFormationPattern;
+	private static int levelNumber;
+
+	public static int getCurrentFormationPattern() {
+		return currentFormationPattern;
+	}
+
+	/**
+	 * This method is called in the game loop to check if the level is finished.
+	 * Currently very empty (for testing)
+	 * @return
+	 */
+	public static void updateLevel() {
+		// TODO Auto-generated method stub
+		Random rnd=new Random();
+		Iterator<Enemy> enemyExistanceCheck = Enemy.enemyIterator();
+		if(!enemyExistanceCheck.hasNext()){
+        	EnemyFormation.createFormation();
+        	currentFormationPattern=rnd.nextInt(EnemyPatterns.patternCount());
+		}
+	}
+	
+	
+}
