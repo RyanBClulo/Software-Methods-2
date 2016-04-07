31515fea59d035fcc1a84623c050ee81b82c15f5
RyanBClulo <ryan.clulo@marquette.edu>
2016-04-07 15:04:36 -0500

    added HighScores text file

--- Software Methodologies Project/src/score/Score.java
+++ Software Methodologies Project/src/score/Score.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Score {
+	private static int currentScore=0;
+	
 	public static void readScores() throws FileNotFoundException{
 		@SuppressWarnings("resource")
 		Scanner inputScores =new Scanner(new File("scores.txt"));
