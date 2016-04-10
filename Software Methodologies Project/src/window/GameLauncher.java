03f0355f3b82e60e6cc0de573bf8fc382727b22a
Franco Reda <FrancoReda95@gmail.com>
2016-04-10 18:44:27 -0500

    Cleaned up code

--- Software Methodologies Project/src/window/GameLauncher.java
+++ Software Methodologies Project/src/window/GameLauncher.java
@@ -1,6 +1,6 @@
 package window;
 /**
- * This is the main class for the SMP-X game.  
+ * This is the main class for the SMP-X game.
  * 
  * @author Ryan Clulo
  * @author Blake Golliday
@@ -12,8 +12,11 @@ package window;
  */
 public class GameLauncher {
 	
+	public static int gameWidth		= 800;
+	public static int gameHeight	= 950;
+	
 	public static void main(String[] args){
-		MainWindow game = new MainWindow("SMP-eXtreme",800,950);
+		MainWindow game = new MainWindow("SMP-eXtreme",gameWidth,gameHeight);
 		game.start();
 	}
 }
